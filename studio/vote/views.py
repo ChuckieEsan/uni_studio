507a3db091f2098ef55a073ff489df9be4c7fdbc
NathanL <tzy15368@outlook.com>
2020-12-05 00:40:30 +0800

    fully functional voting except for auth

--- studio/vote/views.py
+++ studio/vote/views.py
@@ -1,8 +1,10 @@
 from studio.vote import vote
 from studio.models import VoteInfo,VoteCandidates,VoteVotes,db
-from flask import url_for,redirect,render_template,request
+from studio.utils.captcha_helper import get_captcha_and_img
+from flask import url_for,redirect,render_template,request,flash,session,jsonify
 from faker import Faker
 import json
+import time
 f=Faker(locale='zh_CN')
 @vote.route("/")
 def root():
@@ -16,19 +18,43 @@ def root():
 def vote_page(vote_id):
     candidate_all = VoteCandidates.query.filter(VoteCandidates.vote_id==vote_id).all()
     vote_info = VoteInfo.query.filter(VoteInfo.id==vote_id).first_or_404()
+    session['captcha_time'] = int(time.time())+10*60#10分钟
+    session['captcha_str'],captcha_b64 = get_captcha_and_img()
     return render_template(
         'vote_vote_page.html',
         candidate_all=candidate_all,
         vote_info=vote_info,
+        captcha_b64 =captcha_b64
     )
-
+@vote.route('/captcha',methods=['GET','POST'])
+def check_captcha():
+    if request.method=='GET':
+        session['captcha_str'],captcha_64 = get_captcha_and_img()
+        return captcha_64
+    if request.method=='POST':
+        jsoninput = request.get_json()
+        if not session.get('captcha_str'):
+            session['captcha_time'] = int(time.time())+10*60 
+            session['captcha_str'],captcha_64 = get_captcha_and_img()
+            return jsonify({"success":False,"captcha_b64":captcha_64,"data":"无有效验证码"})
+        if int(time.time())>session['captcha_time']:
+            session['captcha_time'] = int(time.time())+10*60 
+            session['captcha_str'],captcha_64 = get_captcha_and_img()
+            return jsonify({"success":False,"captcha_b64":captcha_64,"data":"验证码超时"})
+        if session['captcha_str']!=jsoninput.get('captcha'):
+            session['captcha_time'] = int(time.time())+10*60 
+            session['captcha_str'],captcha_64 = get_captcha_and_img()
+            return jsonify({"success":False,"captcha_b64":captcha_64,"data":"验证码错误"})
+        else:
+            return jsonify({"success":True})
+    
 @vote.route('/<int:vote_id>',methods=["POST"])
 def vote_handler(vote_id):
     voted = VoteVotes.query.filter(VoteVotes.ip==request.remote_addr).filter(VoteVotes.vote_id==vote_id).first()
     if voted:
         print('voted!!')
-        pass
-        #return "volted!"
+        flash("您已投过票！")
+        return render_template('vote_result.html',vote_id=vote_id)
     vote_list = request.get_json()
     print(request.form.getlist('candidates'))
     vote_list = request.form.getlist('candidates')
