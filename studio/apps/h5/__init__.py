4037f80840a5c451aa6ede3830aac7742ab27d3f
NathanL <tzy15368@outlook.com>
2021-04-13 00:54:21 +0800

    feat: add card page

--- studio/apps/h5/__init__.py
+++ studio/apps/h5/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
+import random
 h5 = Blueprint("h5",__name__,template_folder="templates",static_folder="static")
 
 @h5.route('/72years',methods=['GET','POST'])
@@ -7,4 +8,12 @@ def celeb_72_form():
 
 @h5.route('/72years/card',methods=['GET','POST'])
 def celeb_72_card():
-    return render_template('dut72years_card.html')
+    data = dict(request.values)
+    back_images = ['orange.png']
+    if data['wish'] =='others':
+        data['wish'] = data['wish_other']
+    image = random.sample(back_images,1)
+
+
+    return render_template('dut72years_card.html',data=data,image=image)
+
