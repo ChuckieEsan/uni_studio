710a0d8c5732ca9f935731bf5d790c6ef8fea911
NathanL <tzy15368@outlook.com>
2021-04-13 11:53:53 +0800

    feat(h5):add share and reload

--- studio/apps/h5/__init__.py
+++ studio/apps/h5/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,make_response
 import random
 h5 = Blueprint("h5",__name__,template_folder="templates",static_folder="static")
 
@@ -9,11 +9,16 @@ def celeb_72_form():
 @h5.route('/72years/card',methods=['GET','POST'])
 def celeb_72_card():
     data = dict(request.values)
-    back_images = ['orange.png']
+    back_images = ['orange.png','blue.png','gold.png','green.png','purple.png','red.png','silver.png']
     if data['wish'] =='others':
         data['wish'] = data['wish_other']
     image = random.sample(back_images,1)
 
-
-    return render_template('dut72years_card.html',data=data,image=image)
+    
+    res = make_response(render_template('dut72years_card.html',data=data,image=image))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Method'] = '*'
+    res.headers['Access-Control-Allow-Headers'] = '*'
+    return res
+    #return 
 
