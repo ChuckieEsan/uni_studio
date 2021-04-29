f979e98e8af7f636ffa11d3d86ea3436c471b53f
Nathan <40847682+tzY15368@users.noreply.github.com>
2021-04-29 12:48:42 +0800

    feat: update models for global notifications

--- studio/models/__init__.py
+++ studio/models/__init__.py
@@ -5,8 +5,9 @@ from .postcard import db, PostcardCards, PostcardRoles, PostcardTemplates, Postc
 from .user import UserUsers, UserRoles
 from .route import RouteInterceptors
 from .vol_time import VolTime
-from .enroll import EnrollCandidates,EnrollForms
-from .chat import ChatBody,ChatHead
+from .enroll import EnrollCandidates, EnrollForms
+from .chat import ChatBody, ChatHead
+from .notification import GlobalNotifications
 __all__ = [db, VoteInfo, VoteVotes, VoteCandidates, IssuesIssues, IssueTypes,
            PostcardCards, PostcardRoles, PostcardTemplates, PostcardUsers, UserRoles, UserUsers,
-           RouteInterceptors, VolTime, EnrollForms, EnrollCandidates,ChatHead,ChatBody]
+           RouteInterceptors, VolTime, EnrollForms, EnrollCandidates, ChatHead, ChatBody, GlobalNotifications]
---
+++ studio/models/notification.py
@@ -0,0 +1,9 @@
+from .base import db
+from .mixins import TimestampMixin
+
+class GlobalNotifications(db.Model,TimestampMixin):
+    __tablename__ = "notifications_global"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    valid_until = db.Column(db.DateTime(True), default=db.func.now(), nullable=False)
+    created_by = db.Column(db.Integer)
