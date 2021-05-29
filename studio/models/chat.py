37e4ff68621850f9f41e829e476dac377c6f1548
Nathan <40847682+tzY15368@users.noreply.github.com>
2021-05-29 17:01:23 +0800

    feat: add chat room

--- studio/models/chat.py
+++ studio/models/chat.py
@@ -7,8 +7,10 @@ class ChatHead(db.Model,TimestampMixin):
     id = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.Integer)
     second = db.Column(db.Integer)
+    first_alias = db.Column(db.Text)
+    second_alias = db.Column(db.Text)
     def __init__(self,*args,**kwargs):
-        super(ChatBody, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
 class ChatBody(db.Model,TimestampMixin):
     __tablename__ = "chat_body"
@@ -18,5 +20,5 @@ class ChatBody(db.Model,TimestampMixin):
     msg_to = db.Column(db.Integer,nullable=False)
     msg_text = db.Column(db.Text) 
     def __init__(self,*args,**kwargs):
-        super(ChatBody, self).__init__(**kwargs)
+        super().__init__(**kwargs)
     pass
--- studio/utils/send_chat.py
+++ studio/utils/send_chat.py
@@ -1,20 +1,26 @@
 from flask import g
 from studio.models import ChatBody,ChatHead,db
 
-def send_chat(from_id='',to_id='',text=''):
+def send_chat(from_id='',to_id='',text='',head=-1):
     b = ChatBody(
         msg_from=from_id,
         msg_to=to_id,
-        msg_text =text
+        msg_text =text,
+        head=head
         )
     db.session.add(b)
     db.session.commit()
-    return
+    return b
 
-def start_chat(first,second):
+def start_chat(first,second,first_alias=None,second_alias=None):
+    head = ChatHead.query.filter((ChatHead.first==g.user.id)|(ChatHead.second==g.user.id)).first()
+    if head:
+        return head.id
     h = ChatHead(
         first=first,
-        second=second
+        second=second,
+        first_alias=first_alias,
+        second_alias=second_alias
     )
     db.session.add(h)
     db.session.commit()
