6429a5612735341754a833762381a7a6e58eb4bc
Flinx-LY <2102095923@qq.com>
2021-11-19 11:33:05 +0800

    feat: 新增h5 card“专属证书”

--- studio/apps/h5/__init__.py
+++ studio/apps/h5/__init__.py
@@ -1,25 +1,44 @@
-from flask import Blueprint,render_template,request,make_response,current_app
+from flask import Blueprint, render_template, request, make_response, current_app
 import random
-h5 = Blueprint("h5",__name__,template_folder="templates",static_folder="static")
+h5 = Blueprint("h5", __name__, template_folder="templates",
+               static_folder="static")
 
-@h5.route('/72years',methods=['GET','POST'])
+
+@h5.route('/72years', methods=['GET', 'POST'])
 def celeb_72_form():
     return render_template('dut72years_form.html')
 
-@h5.route('/72years/card',methods=['GET','POST'])
+
+@h5.route('/72years/card', methods=['GET', 'POST'])
 def celeb_72_card():
     data = request.values.to_dict()
     current_app.logger.info(data)
-    back_images = ['orange.png','blue.png','gold.png','green.png','purple.png','red.png','silver.png']
-    if data['wish'] =='others':
+    back_images = ['orange.png', 'blue.png', 'gold.png',
+                   'green.png', 'purple.png', 'red.png', 'silver.png']
+    if data['wish'] == 'others':
         data['wish'] = data['wish_other']
-    image = random.sample(back_images,1)
+    image = random.sample(back_images, 1)
 
-    
-    res = make_response(render_template('dut72years_card.html',data=data,image=image))
+    res = make_response(render_template(
+        'dut72years_card.html', data=data, image=image))
     res.headers['Access-Control-Allow-Origin'] = '*'
     res.headers['Access-Control-Allow-Method'] = '*'
     res.headers['Access-Control-Allow-Headers'] = '*'
     return res
-    #return 
+    # return
 
+
+@h5.route('/cert', methods=['GET', 'POST'])
+def cert_form():
+    return render_template('cert_form.html')
+
+
+@h5.route('/cert/card', methods=['GET', 'POST'])
+def cert_card():
+    data = request.values.to_dict()
+
+    res = make_response(render_template('cert_card.html', data=data))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Method'] = '*'
+    res.headers['Access-Control-Allow-Headers'] = '*'
+    return res
